07ef7aba1978f84018a3b3f72b2b03b2e0dba8ee
Dominik <domorozin@gmail.com>
2025-08-15 19:40:21 +0200

    add no args constructor in ExerciseApiDTO

--- workout-service/src/main/java/com/gainlog/workoutservice/dto/ExerciseApiDTO.java
+++ workout-service/src/main/java/com/gainlog/workoutservice/dto/ExerciseApiDTO.java
@@ -2,11 +2,13 @@ package com.gainlog.workoutservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ExerciseApiDTO {
     private String id;
     private String name;
