ec6a7c3905b49c4c6815a9d540e1ee691ee34720
Dominik <domorozin@gmail.com>
2025-08-24 16:05:35 +0200

    remove extra import

--- auth-service/src/main/java/com/gainlog/authservice/model/dto/RegisterUserDTO.java
+++ auth-service/src/main/java/com/gainlog/authservice/model/dto/RegisterUserDTO.java
@@ -1,7 +1,6 @@
 package com.gainlog.authservice.model.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
