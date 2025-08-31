6cbd49d86431411b86718adb2e94dab7b3c22414
Dominik <domorozin@gmail.com>
2025-08-31 15:58:01 +0200

    add run-tests job

--- workout-service/src/test/java/com/gainlog/workoutservice/WorkoutServiceApplicationTests.java
+++ workout-service/src/test/java/com/gainlog/workoutservice/WorkoutServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package com.gainlog.workoutservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class WorkoutServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        // This test will pass if the application context loads successfully
     }
-
 }
