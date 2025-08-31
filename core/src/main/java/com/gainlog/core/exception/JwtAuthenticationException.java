255b6fdf316243f65d12b580253aa3b88ab5df3d
Dominik <domorozin@gmail.com>
2025-08-31 11:55:09 +0200

    remove maven build, add filters for core and proto common modules

--- core/src/main/java/com/gainlog/core/exception/JwtAuthenticationException.java
+++ core/src/main/java/com/gainlog/core/exception/JwtAuthenticationException.java
@@ -3,9 +3,6 @@ package com.gainlog.core.exception;
 import org.springframework.security.core.AuthenticationException;
 
 public class JwtAuthenticationException extends AuthenticationException {
-    public JwtAuthenticationException(String msg) {
-        super(msg);
-    }
 
     public JwtAuthenticationException(String msg, Throwable cause) {
         super(msg, cause);
